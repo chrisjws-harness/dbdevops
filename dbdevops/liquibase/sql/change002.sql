1776e6977abafe5ac2c072da7961c97c943ce680
Chris Storz <83615348+chrisjws-harness@users.noreply.github.com>
2025-04-21 15:28:21 -0700

    Update change002.sql

--- dbdevops/liquibase/sql/change002.sql
+++ dbdevops/liquibase/sql/change002.sql
@@ -1,2 +1,2 @@
 INSERT INTO public.contacts (first_name, last_name, email, phone, address) VALUES
-  ('David',  'Davis',   'david.davis@example.org',   '+1-555-909-0069', '658 Cedar Ln, Mountainview, Ca 80402');
+  ('Nick',  'Knack',   'nick@example.org',   '+1-555-909-0369', '658 Cedar Ln, Mountainview, Ca 80402');
