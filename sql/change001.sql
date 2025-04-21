81674fa361e3016c29561602209fd367f5db8001
Chris Storz <83615348+chrisjws-harness@users.noreply.github.com>
2025-04-21 14:06:15 -0700

    Delete sql directory

--- sql/change001.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO public.contacts (first_name, last_name, email, phone, address) VALUES
-  ('David',  'Davis',   'david.davis@example.org',   '+1-555-909-0069', '658 Cedar Ln, Mountainview, Ca 80402');
