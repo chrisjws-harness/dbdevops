1d5afd2eb13ece4c545b666c8c4f47ae32b93d5b
Chris Storz <83615348+chrisjws-harness@users.noreply.github.com>
2025-04-21 15:41:09 -0700

    Create change003.sql

---
+++ dbdevops/liquibase/sql/change003.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.contacts (first_name, last_name, email, phone, address) VALUES
+  ('Walrus',  'Starky',   'ng@example.org',   '+1-555-909-3369', '658 Cedar Ln, Mountainview, Ca 80402');
